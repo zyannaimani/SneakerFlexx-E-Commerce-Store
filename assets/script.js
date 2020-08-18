6e4a77676637d71aa6086a0bf044cd4f80ac4aa2
Zyanna Imani <49369945+zyannaimani@users.noreply.github.com>
2020-08-18 08:48:58 -0400

    Delete script.js

--- assets/script.js
+++
@@ -1,9 +0,0 @@
-
-var toggleButton = document.getElementById("nav-bars")
-var navMenuMobile = document.getElementById("nav-menu-mobile")
-
-toggleButton.addEventListener("click",()=>{
-    navMenuMobile.classList.toggle("hide")
-    navMenuMobile.classList.toggle("active")
-    console.log("here i ammmm!!!")
-})
